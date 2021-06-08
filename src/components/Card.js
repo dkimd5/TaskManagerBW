e90c1b6a8ad3d22117bf4498fce3d2fa835aa9a9
dkimd5 <dkimd5@mail.ru>
2021-06-08 13:49:02 +0300

    task 2 complited

--- src/components/Card.js
+++ src/components/Card.js
@@ -93,7 +93,14 @@ export const Card = ({ reward, task }) => {
 
          {current.matches("taskcomplite") &&
             <div className='carditem-complite'>
+               <svg className='carditem-circle-img' width="122" height="122" viewBox="0 0 122 122" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <circle cx="61" cy="61" r="60" stroke="#FF8933" strokeDasharray="5 5" />
+               </svg>
+               <svg className='carditem-welldone-img' width="67" height="51" viewBox="0 0 67 51" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M21.75 40.375L6 24.625L0.75 29.875L21.75 50.875L66.75 5.875L61.5 0.625L21.75 40.375Z" fill="#FF8933" />
+               </svg>
                <p className='carditem-complite-text'>Well done!</p>
+               <p className='carditem-complite-points'>You earned { reward } Help Coins! </p>
             </div>
          }
       </li>
