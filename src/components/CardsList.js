2f00759215e2ce5e7473c338d79c3e10e49b6463
dkimd5 <dkimd5@mail.ru>
2021-06-14 10:19:06 +0300

    file-loader added for imgs

--- src/components/CardsList.js
+++ src/components/CardsList.js
@@ -21,19 +21,19 @@ const initialCards = [
 ];
 
 const cardsHistory = {
-   [today]: [
-      { reward: 125, task: "Find dad's wallet", date: today },
-      { reward: 100, task: "Put away old toys to white boxes on the balkoney", date: today },
-      { reward: 75, task: "Wash the dishes", date: today },
-   ],
-   [yesterday]: [
-      { reward: 75, task: "Wash the dishes", date: yesterday },
-      { reward: 50, task: "Water plants on the second floor", date: yesterday },
-   ],
-   [beforeYesterday]: [
-      { reward: 100, task: "Help grandma with shopping", date: beforeYesterday },
-      { reward: 50, task: "Water plants", date: beforeYesterday },
-   ]
+   // [today]: [
+   //    { reward: 125, task: "Find dad's wallet", date: today },
+   //    { reward: 100, task: "Put away old toys to white boxes on the balkoney", date: today },
+   //    { reward: 75, task: "Wash the dishes", date: today },
+   // ],
+   // [yesterday]: [
+   //    { reward: 75, task: "Wash the dishes", date: yesterday },
+   //    { reward: 50, task: "Water plants on the second floor", date: yesterday },
+   // ],
+   // [beforeYesterday]: [
+   //    { reward: 100, task: "Help grandma with shopping", date: beforeYesterday },
+   //    { reward: 50, task: "Water plants", date: beforeYesterday },
+   // ]
 }
 
 
@@ -58,10 +58,15 @@ export const CardsList = () => {
                )) }
             </ul>
          )
-      } else {
+      } else if (Object.keys(cardsArray).length == 0) {
+         return (
+            <div className="emptyHistory"></div>
+         )
+      }
+      else {
          return (Object.keys(cardsArray).map(date => (
             <div key={ Math.random() }>
-               <h3>{ date }</h3>
+               <h3>{ today === date ? <span>Today</span> : "" } { date }</h3>
                <ul className="cardslist">
                   {
                      cardsArray[date].map((card, index) => (
