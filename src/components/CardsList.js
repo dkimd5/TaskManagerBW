ea4055df44bbad15364b43f6f52bbc4cfbf09a39
dkimd5 <dkimd5@mail.ru>
2021-06-17 12:10:32 +0300

    firebase and firebase-react added

--- src/components/CardsList.js
+++ src/components/CardsList.js
@@ -21,19 +21,19 @@ const initialCards = [
 ];
 
 const cardsHistory = {
-   // [today]: [
-   //    { reward: 125, task: "Find dad's wallet", date: today },
-   //    { reward: 100, task: "Put away old toys to white boxes on the balkoney", date: today },
-   //    { reward: 75, task: "Wash the dishes", date: today },
-   // ],
-   // [yesterday]: [
-   //    { reward: 75, task: "Wash the dishes", date: yesterday },
-   //    { reward: 50, task: "Water plants on the second floor", date: yesterday },
-   // ],
-   // [beforeYesterday]: [
-   //    { reward: 100, task: "Help grandma with shopping", date: beforeYesterday },
-   //    { reward: 50, task: "Water plants", date: beforeYesterday },
-   // ]
+   [today]: [
+      { reward: 125, task: "Find dad's wallet", date: today },
+      { reward: 100, task: "Put away old toys to white boxes on the balkoney", date: today },
+      { reward: 75, task: "Wash the dishes", date: today },
+   ],
+   [yesterday]: [
+      { reward: 75, task: "Wash the dishes", date: yesterday },
+      { reward: 50, task: "Water plants on the second floor", date: yesterday },
+   ],
+   [beforeYesterday]: [
+      { reward: 100, task: "Help grandma with shopping", date: beforeYesterday },
+      { reward: 50, task: "Water plants", date: beforeYesterday },
+   ]
 }
 
 
