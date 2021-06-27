07eaadb15220c3013c9def3a63720c56296f4378
dkimd5 <dkimd5@mail.ru>
2021-06-27 16:49:59 +0300

    cardoptions side added

--- src/components/AddCard.js
+++ src/components/AddCard.js
@@ -29,11 +29,31 @@ export const AddCard = () => {
    const [current, send] = useMachine(addCardMachine);
 
    return (
-      <li className="addCard">
+      // <li className="addCard">
+      <>
          {
-            current.matches("addcard") && 
-            <div className="addCard-plus"></div>
+            current.matches("addcard") &&
+            <li
+               className="addCard"
+               onClick={ () => { send('START_CARD') } }
+            >
+            </li>
          }
-      </li>
+         {
+            current.matches("cardoptions") &&
+            <li className="card-options">
+               <div className="card-options-wrp">
+                  <h3>New housework task</h3>
+                  <label for="cardText">Title</label>
+                  <input id="cardText" type="text"></input>
+                  <label for="cardReward">Reward</label>
+                  <input id="cardReward" type="text"></input>
+               </div>
+               <button>Cancel</button>
+               <button>Create</button>
+            </li>
+         }
+      </>
+      // </li>
    )
 }
