4aca2fb20e0d6004aa1305e5f1e72239fe78a720
dkimd5 <dkimd5@mail.ru>
2021-11-11 20:43:49 +0300

    starting firebase work

--- src/index.js
+++ src/index.js
@@ -1,22 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { App } from "./components/App";
-import firebase from "firebase";
 import { Provider } from "react-redux";
 import { store } from "./store/store";
-
-let firebaseConfig = {
-   apiKey: "AIzaSyBuZc1sqAqAQt2eCDOSAymR38KngE2zDs4",
-   authDomain: "taskmanagerbw.firebaseapp.com",
-   projectId: "taskmanagerbw",
-   storageBucket: "taskmanagerbw.appspot.com",
-   messagingSenderId: "35471273360",
-   appId: "1:35471273360:web:ac6c85a8ef43e6a76e9715",
-   measurementId: "G-TLQ5G5VRZT"
-};
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-firebase.analytics();
 
 
 ReactDOM.render(
