44ff96ed15c0a21c98e98c517db487867e4ceda8
dkimd5 <dkimd5@mail.ru>
2021-11-16 13:39:15 +0300

    getCardCollection middleware created, actions for middlewares at card list added

--- src/store/cards/actions.js
+++ src/store/cards/actions.js
@@ -1,26 +1,82 @@
-import { doc, setDoc, addDoc, collection } from "firebase/firestore";
+import { doc, setDoc, getDoc, Timestamp } from "firebase/firestore";
 import db from "/src/firebase";
-export const ADD_CARD = 'CARDS_LIST::ADD_CARD';
+
+export const ADD_CARD = 'CARD_LIST::ADD_CARD';
+export const CARD_COLLECTION_INIT = 'CARD_LIST::CARD_COLLECTION_INIT';
+export const CARD_COLLECTION_INIT_SUCCESS = 'CARDS_LIST::CARD_COLLECTION_INIT_SUCCESS';
+export const CARD_COLLECTION_INIT_FAILURE = 'CARDS_LIST::CARD_COLLECTION_INIT_FAILURE';
+export const CARD_COLLECTION_GET_REQUEST = 'CARD_LIST::CARD_COLLECTION_GET_REQUEST';
+export const CARD_COLLECTION_GET_SUCCESS = 'CARD_LIST::CARD_COLLECTION_GET_SUCCESS';
+export const CARD_COLLECTION_GET_FAILURE = 'CARD_LIST::CARD_COLLECTION_GET_FAILURE';
+
 
 export const addCard = newCard => ({
    type: ADD_CARD,
    payload: newCard,
 })
 
-export const createCardsCollection = () => async (dispatch) => {
+export const cardCollectionInit = () => ({
+   type: CARD_COLLECTION_INIT,
+})
+
+export const cardCollectionInitFailure = (error) => ({
+   type: CARD_COLLECTION_INIT_FAILURE,
+   payload: error,
+})
+
+export const cardCollectionInitSuccess = () => ({
+   type: CARD_COLLECTION_INIT_SUCCESS,
+})
+
+export const cardCollectionGetRequest = () => ({
+   type: CARD_COLLECTION_INIT,
+})
+
+export const cardCollectionGetFailure = (error) => ({
+   tyep: CARD_COLLECTION_GET_FAILURE,
+   payload: error,
+})
+
+export const cardCollectionGetSuccess = (cardList) => ({
+   type: CARD_COLLECTION_GET_SUCCESS,
+   payload: cardList,
+})
+
+export const initCardCollection = () => async (dispatch) => {
+   dispatch(cardCollectionInit())
+
    try {
       const docRef = await setDoc(doc(db, "cards", "cardsList"), {
          cardsList: [
-            { reward: 125, task: "Find dad's wallet", date: '15.11.21' },
-            { reward: 100, task: "Put away old toys to white boxes on the balkoney", date: '15.11.21' },
-            { reward: 75, task: "Wash the dishes", date: '15.11.21' },
-            { reward: 50, task: "Make your bed in the morning", date: '14.11.21' },
-            { reward: 50, task: "Fix the pillowcase", date: '15.11.21' },
-            { reward: 50, task: "Water plants", date: '14.11.21' },
+            { reward: 125, task: "Find dad's wallet", date: Timestamp.fromDate(new Date("November 15, 2021")) },
+            { reward: 100, task: "Put away old toys to white boxes on the balkoney", date: Timestamp.fromDate(new Date("November 13, 2021")) },
+            { reward: 75, task: "Wash the dishes", date: Timestamp.fromDate(new Date("November 15, 2021")) },
+            { reward: 50, task: "Make your bed in the morning", date: Timestamp.fromDate(new Date("November 14, 2021")) },
+            { reward: 50, task: "Fix the pillowcase", date: Timestamp.fromDate(new Date("November 15, 2021")) },
+            { reward: 50, task: "Water plants", date: Timestamp.fromDate(new Date("November 14, 2021")) },
          ]
       });
+
+      dispatch(cardCollectionInitSuccess())
       console.log("Document written with ID: ", docRef?.id);
    } catch (e) {
+      dispatch(cardCollectionInitFailure(e.message))
       console.error("Error adding document: ", e);
+   }
+}
+
+export const getCardCollection = () => async (dispatch) => {
+   dispatch(cardCollectionGetRequest());
+
+   try {
+      const docRef = doc(db, "cards", "cardsList");
+      const docSnap = await getDoc(docRef);
+
+      const data = docSnap;
+      console.log(data);
+
+      dispatch(cardCollectionGetSuccess(data))
+   } catch (err) {
+      dispatch(cardCollectionGetFailure(err.message))
    }
 }
